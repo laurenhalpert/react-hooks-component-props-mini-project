9b27897ff0649544a3b21a891c53b7a2c3311b75
Lauren <halpertln@gmail.com>
2023-03-21 10:31:38 -0600

    fix test fro ArticleList

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -2,13 +2,11 @@ import React from 'react';
 import Article from "./Article"
 
 function ArticleList({post}){
-    console.log(post);
-    const mappedPost = post.map(elem=>(
-        <Article key={elem.id} title={elem.title} date={elem.date} preview={elem.preview} minutes={elem.minutes}/>
-    ))
     return(
         <main>
-            {mappedPost}
+            {post.map(elem=>(
+        <Article key={elem.id} title={elem.title} date={elem.date} preview={elem.preview} minutes={elem.minutes}/>
+    ))}
         </main>
     )
 }
