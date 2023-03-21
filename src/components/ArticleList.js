6f02ded9c32b1c7b17ac135e9178bc2b002b8ccf
Lauren <halpertln@gmail.com>
2023-03-21 10:42:36 -0600

    another fix for ArticleList

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Article from "./Article"
 
-function ArticleList({post}){
-    let arr = post;
+function ArticleList({posts}){
+    let arr = posts;
     console.log(arr);
     return(
         <main>
             {arr.map(elem=>(
-        <Article key={elem.id} title={elem.title} date={elem.date} preview={elem.preview} minutes={elem.minutes}/>
-    ))}
+                <Article key={elem.id} title={elem.title} date={elem.date} preview={elem.preview} minutes={elem.minutes}/>
+            ))}
         </main>
     )
 }
