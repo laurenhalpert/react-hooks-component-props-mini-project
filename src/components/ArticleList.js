ab2e76a925b9394001fb0e5c05d18ae3a2c4f869
Lauren <halpertln@gmail.com>
2023-03-21 10:36:55 -0600

    new fix ArticleList

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -2,9 +2,11 @@ import React from 'react';
 import Article from "./Article"
 
 function ArticleList({post}){
+    let arr = post;
+    console.log(arr);
     return(
         <main>
-            {post.map(elem=>(
+            {arr.map(elem=>(
         <Article key={elem.id} title={elem.title} date={elem.date} preview={elem.preview} minutes={elem.minutes}/>
     ))}
         </main>
