c8dfd583bdb1c57aefcc095db66021c1d145f057
Lauren <halpertln@gmail.com>
2023-03-21 10:19:38 -0600

    lab with bonuses

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,10 +1,44 @@
 import React from 'react'
 
-function Article({title, date="January 1, 1970", preview}) {
+function Article({title, date="January 1, 1970", preview, minutes}) {
+    let num = 0;
+    const coffee = []
+    let joinedStr="";
+    const bentos =[]
+    
+    if (minutes <30) {
+        num = Math.ceil(minutes/5)*5
+        function numberCoffees() {
+            let numCoffee = num/5;
+            coffee.length=numCoffee;
+            for (let i =0; i <numCoffee; i++){
+                coffee[i] = "☕️"
+            }
+            joinedStr = coffee.join('')
+        }
+        numberCoffees()
+    } else {
+        num = Math.ceil(minutes/10)*10
+        function numberBentos() {
+            let numBentos = num/10;
+            bentos.length=numBentos;
+            for (let i=0; i<numBentos; i++){
+                bentos[i] ="🍱"
+            }
+            joinedStr = bentos.join('')
+        }
+        numberBentos()
+    }
+
+   
+    
+    
+    
+    console.log(num)
     return(
         <article>
             <h3>{title}</h3>
-            <small>{date}</small>
+            <small>{date}<span> | {joinedStr} {minutes} min. read</span></small>
             <p>{preview}</p>
         </article>
     )
--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -4,7 +4,7 @@ import Article from "./Article"
 function ArticleList({post}){
     console.log(post);
     const mappedPost = post.map(elem=>(
-        <Article key={elem.id} title={elem.title} date={elem.date} preview={elem.preview}/>
+        <Article key={elem.id} title={elem.title} date={elem.date} preview={elem.preview} minutes={elem.minutes}/>
     ))
     return(
         <main>
